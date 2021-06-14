09284a1473401bb6045b03cf7fd743797f8f8811
alphaday11 <84897027+alphaday11@users.noreply.github.com>
2021-06-14 11:50:58 +0900

    Add files via upload

--- 완성(RSA공개키 암호화, 복호화).py
+++ 완성(RSA공개키 암호화, 복호화).py
@@ -27,31 +27,26 @@ def get_public_key(tot):
 
 m = input("변환 할 문자열을 입력:")
 
-# 두개의 소수 설정
+# 두 소수 p, qp, q 준비
 p = 1117
 q = 1123
-
-print("Two prime numbers(p and q) are:", str(p), "and", str(q))
-
-# 두 소수의 계수 구하기
 n = p*q
 print("n(p*q)=", str(p), "*", str(q), "=", str(n))
+print("Two prime numbers(p and q) are:", str(p), "and", str(q))
 
-# 피함수 값 totient 구하기
+# 피함수 값 구하기
 totient = (p-1)*(q-1)
 print("(p-1)*(q-1)=", str(totient))
 
-# 공개키
+# 공개키(ee)
 e = get_public_key(totient)
 print("공개키 (n, e):("+str(n)+","+str(e)+")")
 
-# 개인키
+# 개인키(dd)
 d = get_private_key(e, totient)
 print("개인키 (n, d):("+str(n)+","+str(d)+")")
 
 
 encrypted_msg = encrypt((e,n), m)
 print('암호화된 메세지 :', ''.join(map(lambda x: str(x), encrypted_msg)))
-
-
 print('복호화된 메세지 :', decrypt((d,n),encrypted_msg))
